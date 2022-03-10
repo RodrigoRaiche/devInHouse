e94332b50f38db1d335143efc3d309104e5563e0
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-03-09 23:04:55 -0300

    Add M1S02-Ex8

---
+++ exercicios 02 - 0703-1103/m1s02-ex8.js
@@ -0,0 +1,6 @@
+var arr = [ 56, true, 'JS', 67, 99, 12, false, 88];
+
+console.log('Primeiro elemento               : ' + arr[0]);
+console.log('Último elemento                 : ' + arr[arr.length - 1]);
+console.log('Terceiro elemento               : ' + arr[2]);
+console.log('Tipo de valor do quinto elemento: ' + typeof arr[4]);
