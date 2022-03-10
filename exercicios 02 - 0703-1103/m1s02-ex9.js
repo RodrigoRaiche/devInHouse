dd3b071b4ba88fdd17292660ea3fdfff086f88dc
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-03-10 18:30:51 -0300

    Add M1S02-Ex9

---
+++ exercicios 02 - 0703-1103/m1s02-ex9.js
@@ -0,0 +1,13 @@
+var notas = [
+    [10, 8.5, 5.5, 9.5],
+    [8.5, 7, 7.5, 6],
+    [8, 9, 10, 7.5],
+    [9, 9.5, 10, 8]
+];
+
+var soma = notas[0][0] + notas[1][0]  + notas[2][0] + notas[3][0] ;
+
+
+console.log('Primeiro elemento do terceiro array      : ' + notas[2][0]);
+console.log('Segundo elemento do quarto array         : ' + notas[3][1]);
+console.log('Soma das primeiras notas de cada array   : ' + soma);
