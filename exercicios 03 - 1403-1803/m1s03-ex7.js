4a04179a0fe578308f671eea3e2a3afd9f5ebc8c
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-03-17 19:47:17 -0300

    Alter M1S03-Ex7

--- exercicios 03 - 1403-1803/m1s03-ex7.js
+++ exercicios 03 - 1403-1803/m1s03-ex7.js
@@ -1,14 +1,36 @@
-var API = "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/"
+var API1 = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes/"
+var API2 = "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/"
 var endereco = "";
 var nro = 0;
+var localidade = [];
+
 
 let listaPopulacao = document.querySelector("#listaPopulacao");
 listaPopulacao.style.display = "flex";
 
+for (var i = 0; i < 5; i++) {
+    nro = i + 1
+    endereco = API1 + nro;
+    console.log(endereco)
+    fetch(endereco).then(function (resposta) {
+        return resposta.json();
+    }).then(function (json) {
+        console.log(json)
+        manipulaAposRequisicaoRegioes(json)
+    });
+}
+
+function manipulaAposRequisicaoRegioes(json) {
+    const dados = json;
+    console.log(dados);
+    localidade[dados.id - 1] = dados.nome + ' - ' + dados.sigla;
+    console.log(localidade[dados.id - 1]);
+}
+
 
 for (var i = 0; i < 5; i++) {
     nro = i + 1
-    endereco = API + nro;
+    endereco = API2 + nro;
     console.log(endereco)
     fetch(endereco).then(function (resposta) {
         return resposta.json();
@@ -37,7 +59,7 @@ function manipulaAposRequisicao(json) {
     const p1List = document.createElement("p");
     p1List.innerHTML = "Horário: " + dados.horario;
     const p2List = document.createElement("p");
-    p2List.innerHTML = "Localidade: " + dados.localidade;
+    p2List.innerHTML = "Localidade: " + dados.localidade + ' - ' + localidade[dados.localidade -1];
     const p3List = document.createElement("p");
     p3List.innerHTML = "Incremento Populacional: " + dados.projecao.periodoMedio.incrementoPopulacional;
     const p4List = document.createElement("p");
