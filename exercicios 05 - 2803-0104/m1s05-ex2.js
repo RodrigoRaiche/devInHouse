823212da56cb60cc510a98f70ae034f5de39a5fb
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-03-29 19:41:07 -0300

    Add m1s05-Ex2.js M1S05-Ex2.html

---
+++ exercicios 05 - 2803-0104/m1s05-ex2.js
@@ -0,0 +1,40 @@
+const btnProcess = document.querySelector('#process');
+
+
+
+btnProcess.addEventListener("click", event => {
+    const inputs = document.querySelectorAll('input');
+
+    const spans = document.querySelectorAll('span').forEach((span) => {
+        span.remove();
+    });
+    
+    const arrayDentroFaixa0e1 = [...inputs].filter(input => {
+        return (input.length != 0 && (parseFloat(input.value) >= 0 && parseFloat(input.value) <= 1));
+    }).forEach((input) => {
+
+        input.style.border = '2px inset  rgb(118, 118, 118)';
+        const divElement = document.getElementById(input.id).parentNode;
+
+        const spanResultado = document.createElement('span');
+        const corDinamica = '#' + Math.floor((parseFloat(input.value)) * 16777215).toString(16);
+
+        spanResultado.innerHTML = corDinamica;
+        spanResultado.style.backgroundColor = corDinamica;
+        divElement.append(spanResultado)
+
+    })
+
+    const arrayForaFaixa0e1 = [...inputs].filter(input => {
+        return (input.length === 0 || parseFloat(input.value) < 0 || parseFloat(input.value) > 1);
+    }).forEach(input => {
+        console.log(input.length)
+        input.style.border = '2px solid red'
+        
+    });
+
+}
+
+
+);
+
