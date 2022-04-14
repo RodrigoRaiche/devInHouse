85dbe560425aa7f580817186edd5686b90e47eb5
Raiche <101061963+RodrigoRaiche@users.noreply.github.com>
2022-04-14 18:28:11 -0300

    Delete projeto01 directory

--- projeto01/product.js
+++
@@ -1,8 +0,0 @@
-export class product {
-    constructor(id, description) {
-        this.id = id;
-        this.checkbox = false;
-        this.description = description;
-        this.value = parseFloat(0.00);
-    }
-}    
